be6ce2a506a1545957a48953448bb0ca50ed36ed
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-03-24 22:08:36 +0500

    postgresql added

--- config/settings.py
+++ config/settings.py
@@ -90,12 +90,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Uzdev_db',
+        'USER': 'uzdev_admin',
+        'PASSWORD': 'ruxshona',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
